bcfa48deb026e8aefa057ff9f79daa806a8e03a3
Marco Carraro <ma.carraro@proton.me>
2025-03-23 13:55:49 +0100

    Prettier formatting + update every 500 -> 750 ms

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 const $ = (selector) => {
-    return document.querySelector(selector);
-}
+	return document.querySelector(selector);
+};
 
-const hour = $(".hour");
-const dot = $(".dot");
-const min = $(".min");
-const week = $(".week");
+const hour = $('.hour');
+const dot = $('.dot');
+const min = $('.min');
+const week = $('.week');
 let showDot = true;
 
 function update() {
-    showDot = !showDot;
-    const now = new Date();
+	showDot = !showDot;
+	const now = new Date();
 
-    if (showDot) {
-        dot.classList.add("invisible");
-    } else {
-        dot.classList.remove("invisible");
-    }
-    hour.textContent = String(now.getHours())
-        .padStart(2, "0");
-    min.textContent = String(now.getMinutes())
-        .padStart(2, "0");
+	if (showDot) {
+		dot.classList.add('invisible');
+	} else {
+		dot.classList.remove('invisible');
+	}
+	hour.textContent = String(now.getHours()).padStart(2, '0');
+	min.textContent = String(now.getMinutes()).padStart(2, '0');
 
-    Array.from(week.children).forEach(ele => {
-        ele.classList.remove("active");
-    });
-    
-    // Convert Sunday (0) to 6, and shift other days by -1
-    const dayIndex = now.getDay() === 0 ? 6 : now.getDay() - 1;
-    week.children[dayIndex].classList.add("active");
-};
+	Array.from(week.children).forEach((i) => {
+		i.classList.remove('active');
+	});
 
-update()
+	// Convert Sunday (0) to 6, and shift other days by -1
+	const dayIndex = now.getDay() === 0 ? 6 : now.getDay() - 1;
+	week.children[dayIndex].classList.add('active');
+}
 
-setInterval(update, 500)
+update();
+
+setInterval(update, 750);
